11588f0d48338c75c8cca68f0a62c8a2617b64c8
MoKi <61506669+moki929@users.noreply.github.com>
2020-08-08 22:10:43 -0400

    adding files

---
+++ calc.js
@@ -0,0 +1,15 @@
+
+
+var x=2;
+let y=3;
+const z=5;
+
+function addy(x,y) {
+  return x+y;
+}
+
+if (addy(x,y)==z){
+  console.log(x);
+  console.log(y);
+  console.log(z);
+} else { alert ("fail")}
